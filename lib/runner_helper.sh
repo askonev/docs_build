4a528a979ee169bd76b96a32782db4e009061a34
MaxSorokin <askonev007@gmail.com>
2023-01-25 15:54:40 +0400

    Fix _remove_logs methods

--- lib/runner_helper.sh
+++ lib/runner_helper.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-function _remove_logs {
-
-    LOG_FOLDER="$(pwd)/DocumentServer"
-    sudo rm -rf $LOG_FOLDER/data/*
-    sudo rm -rf $LOG_FOLDER/data/.private/*
-    sudo rm -rf $LOG_FOLDER/db/*
-    sudo rm -rf $LOG_FOLDER/lib/*
-    sudo rm -rf $LOG_FOLDER/logs/*
-
+function _remove_logs() {
+  echo "logs dir: $1"
+  sudo rm -r $1/*
+  shift
 }
